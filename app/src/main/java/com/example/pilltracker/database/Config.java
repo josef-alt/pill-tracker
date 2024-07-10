efd54c7d5f1bf32cf3864f55a3c2a53e6fb12295
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-07-10 14:07:54 -0500

    set up constants for db schema

--- app/src/main/java/com/example/pilltracker/database/Config.java
+++ app/src/main/java/com/example/pilltracker/database/Config.java
@@ -4,10 +4,29 @@ package com.example.pilltracker.database;
  */
 public class Config {
     // medications
-    public static final String TABLE_MEDS = "meds";
-    public static final String MEDS_ID = "id";
-    public static final String MEDS_NAME = "name";
+    public static final String MEDS_TABLE = "medications";
+    public static final String MED_ID = "_id";
+    public static final String MED_NAME = "name";
+    public static final String MED_DOSE = "dose";
+    public static final String MED_FREQ = "freq";
 
-    // history
+    public static final String MEDS_LOG_TABLE = "medication_log";
+    public static final String MED_LOG_LOG_ID = "_id";
+    public static final String MED_LOG_MED_ID = "medication_id";
+    public static final String MED_LOG_TIME = "timestamp";
+
+    // measurements
+    public static final String MEASURES_TABLE = "measurements";
+    public static final String MEASURE_ID = "_id";
+    public static final String MEASURE_NAME = "name";
+    public static final String MEASURE_FREQ = "freq";
+
+    public static final String MEASURE_LOG_TABLE = "measurement_log";
+    public static final String MEASURE_LOG_MEASURE_ID = "_id";
+    public static final String MEASURE_LOG_ID = "measurement_id";
+    public static final String MEASURE_LOG_VALUE = "value";
+    public static final String MEASURE_LOG_TIME = "timestamp";
+
     // reminders
+    public static final String REMINDER_VIEW = "reminders";
 }
